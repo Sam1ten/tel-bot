e88cd9756be93f515dd8f8f07c3ebab3918fa0dd
Sam1ten <118332091+Sam1ten@users.noreply.github.com>
2025-03-28 10:49:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import time
 CHROME_DRIVER_PATH = "/usr/local/bin/chromedriver"
 options = Options()
 options.add_argument("--headless=new")
+options.add_argument("--remote-debugging-port=9222")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
